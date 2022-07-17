6750289dc7d6bcfa3bcd6775872d88f749f1571f
Brandon Wu <wu302966@gmail.com>
2022-07-17 16:20:45 -0700

    fixed Lab4B title

--- Project1/Lab4/Lab4B.cpp
+++ Project1/Lab4/Lab4B.cpp
@@ -1,11 +1,11 @@
 /*
 Prolog
-   a. 	Program Description: Array identical checker
+   a. 	Program Description: Duplicate remover
    b. 	Author: Brandon Wu
    c. 	Date: 07-17-22
-   d. 	Input variables: array1, array2
-   e. 	Process Flow: counting algorithm and conditions
-   f. 	Output variables: array1_size, array2_size, equals
+   d. 	Input variables: array1
+   e. 	Process Flow: array parsing
+   f. 	Output variables: array2
 */
 
 #include <iostream>
