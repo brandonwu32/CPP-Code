afe0c807e4cf1a8cce98e636fddcda067e0979c9
Brandon Wu <wu302966@gmail.com>
2022-07-17 22:56:59 -0700

    start lab 5A

---
+++ Project1/Lab5/Lab5A.cpp
@@ -0,0 +1,18 @@
+/*
+Prolog
+   a. 	Program Description: Duplicate remover
+   b. 	Author: Brandon Wu
+   c. 	Date: 07-17-22
+   d. 	Input variables: array1
+   e. 	Process Flow: array parsing
+   f. 	Output variables: array2
+*/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void reverse(double* a, int size)
